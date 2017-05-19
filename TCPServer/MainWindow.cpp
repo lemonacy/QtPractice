bc8e4e6cf3e8db9e41d01cda58ff828946693ebd
dengkuadong <lemonacy@163.com>
2017-05-19 19:18:10 +0800

    tcp/udp fine-tune

--- TCPServer/MainWindow.cpp
+++ TCPServer/MainWindow.cpp
@@ -9,6 +9,7 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow),
     tcpServer(nullptr),
+    tcpSocket(nullptr),
     udpSocket(nullptr)
 {
     ui->setupUi(this);
@@ -22,10 +23,14 @@ MainWindow::MainWindow(QWidget *parent) :
 
     connect(ui->m_startTCPServer, static_cast<void (QPushButton::*)(bool)>(&QPushButton::clicked), this, &MainWindow::startTCPServer);
     connect(ui->m_stopTCPServer, static_cast<void (QPushButton::*)(bool)>(&QPushButton::clicked), this, &MainWindow::stopTCPServer);
+    connect(ui->m_tcpConnect, static_cast<void(QPushButton::*)(bool)>(&QPushButton::clicked), this, &MainWindow::connectTCPServer);
+    connect(ui->m_tcpSendMsg, static_cast<void(QPushButton::*)(bool)>(&QPushButton::clicked), this, &MainWindow::sendTcpMessage);
+    connect(ui->m_tcpClientMsg, &QLineEdit::returnPressed, this, &MainWindow::sendTcpMessage);
 
     connect(ui->m_bindUdp, static_cast<void (QPushButton::*)(bool)>(&QPushButton::clicked), this, &MainWindow::bindUdp);
     connect(ui->m_unbindUdp, static_cast<void (QPushButton::*)(bool)>(&QPushButton::clicked), this, &MainWindow::unbindUdp);
     connect(ui->m_sendUDPMessage, static_cast<void (QPushButton::*)(bool)>(&QPushButton::clicked), this, &MainWindow::sendUdpMessage);
+    connect(ui->m_udpMessage, &QLineEdit::returnPressed, this, &MainWindow::sendUdpMessage);
 }
 
 MainWindow::~MainWindow()
@@ -50,7 +55,7 @@ void MainWindow::startTCPServer()
         ui->m_console->append(QString("%1:%2 connected").arg(socket->peerAddress().toString()).arg(socket->peerPort()));
         connect(socket, &QTcpSocket::readyRead, [socket, this]{
             QString str = socket->readAll();
-            ui->m_console->append(QString("%1:%2 says:%3").arg(socket->peerAddress().toString()).arg(socket->peerPort()).arg(str));
+            ui->m_console->append(QString("%1:%2 says: %3").arg(socket->peerAddress().toString()).arg(socket->peerPort()).arg(str));
             socket->write(QString("Yes? %1").arg(str).toStdString().c_str());
         });
         connect(socket, &QTcpSocket::disconnected, [socket, this]{
@@ -73,6 +78,36 @@ void MainWindow::stopTCPServer()
     }
 }
 
+void MainWindow::connectTCPServer()
+{
+    if (tcpSocket == nullptr)
+    {
+        tcpSocket = new QTcpSocket();
+        tcpSocket->connectToHost(QHostAddress(ui->m_tcpServerAddress->text()), ui->m_tcpServerPort->text().toInt());
+        tcpSocket->waitForConnected();
+        ui->m_tcpConnect->setText("Disconnect");
+    }
+    else
+    {
+        tcpSocket->flush();
+        tcpSocket->close();
+        delete tcpSocket;
+        tcpSocket = nullptr;
+        ui->m_tcpConnect->setText("Connect");
+    }
+}
+
+void MainWindow::sendTcpMessage()
+{
+    if (tcpSocket->state() == QAbstractSocket::ConnectedState)
+    {
+        QString msg = ui->m_tcpClientMsg->text();
+        tcpSocket->write(msg.toUtf8());
+        tcpSocket->waitForBytesWritten();
+        ui->m_console->append(QString("Sent [%1] to %2:%3").arg(msg).arg(tcpSocket->peerAddress().toString()).arg(tcpSocket->peerPort()));
+    }
+}
+
 void MainWindow::bindUdp()
 {
     udpSocket = new QUdpSocket();
@@ -88,7 +123,7 @@ void MainWindow::bindUdp()
             quint16 remotePort;
             datagram.resize(udpSocket->pendingDatagramSize());
             udpSocket->readDatagram(datagram.data(), datagram.size(), &remoteAddress, &remotePort);
-            ui->m_console->append(QString("Received [%1] from %2:%3").arg(QString(datagram)).arg(remoteAddress.toString()).arg(remotePort));
+            ui->m_console->append(QString("%2:%3 says: %1").arg(QString(datagram)).arg(remoteAddress.toString()).arg(remotePort));
         }
     });
 }
@@ -112,5 +147,5 @@ void MainWindow::sendUdpMessage()
 
     udpSocket->writeDatagram(msg.toUtf8(), QHostAddress(address), port);
     udpSocket->flush();
-    ui->m_console->append(QString("Send [%1] to %2:%3").arg(msg).arg(address).arg(port));
+    ui->m_console->append(QString("Sent [%1] to %2:%3").arg(msg).arg(address).arg(port));
 }
--- TCPServer/MainWindow.h
+++ TCPServer/MainWindow.h
@@ -19,11 +19,15 @@ public:
 private:
     Ui::MainWindow *ui;
     QTcpServer *tcpServer;
+    QTcpSocket *tcpSocket;
     QUdpSocket *udpSocket;
 
 private slots:
     void startTCPServer();
     void stopTCPServer();
+    void connectTCPServer();
+    void sendTcpMessage();
+
     void bindUdp();
     void unbindUdp();
     void sendUdpMessage();
